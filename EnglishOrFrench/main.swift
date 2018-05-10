026fc4543219f70bc732bc462970b2857750713b
Zhang Matt <mzhang@lcs.on.ca>
2018-05-10 09:58:10 -0400

    I think my thing works now

--- EnglishOrFrench/main.swift
+++ EnglishOrFrench/main.swift
@@ -7,13 +7,30 @@ import Foundation
 
 // INPUT
 // Collect and filter user input here
+var expectedLines = 0
+
+while true {
+    print("How many lines of text?")
+    guard let givenInput = readLine() else {
+        continue
+    }
+    guard let checkedInput = Int(givenInput) else {
+        continue
+    }
+    if checkedInput < 0 || checkedInput > 10000 {
+        continue
+    }
+    expectedLines = checkedInput
+    break
+}
 
 // PROCESS
 // Implement the primary logic of the problem here
 // Some output may be given here if you desire
 
 // Example of how to collect multiple input lines
-let expectedLines = 4
+var frenchLetters = 0
+var englishLetters = 0
 print("Please enter the \(expectedLines) of text:")
 for _ in 1...expectedLines {
     
@@ -23,13 +40,25 @@ for _ in 1...expectedLines {
         // If someone enters nil input, just skip to the next line
         continue
     }
-    
     // Now we have the line, we can print it out, analyze it as needed, et cetera
-    print(givenLine)
-    
+    for letter in givenLine {
+        switch letter {
+        case "t", "T":
+            englishLetters += 1
+        case "s", "T":
+            frenchLetters += 1
+        default:
+            break
+        }
+    }
 }
 
 // OUTPUT
 // Report results to the user here
 
+if englishLetters > frenchLetters {
+    print("This looks like English.")
+} else {
+    print("This looks like French.")
+}
 
